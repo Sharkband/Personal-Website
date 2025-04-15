82d4b81d9e69c6850b3c51583c97fc05bee5b37d
Lucas Drennan <96320875+Sharkband@users.noreply.github.com>
2025-04-15 17:48:08 -0400

    file fix

--- my-react-website/vite.config.js
+++ my-react-website/vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/*',
+          dest: '.' // copy into root of dist
+        }
+      ]
+    })
+  ],
   server: {
-    host: '0.0.0.0',  // Allow external access
-    port: 80,        // Default Vite port, you can change it if necessary
+    host: '0.0.0.0',
+    port: 80
   },
 })
+
 
